70f59c9f44c65c2caf7c32f0c8269a53f53f98a3
Piotr Pałek <piotr.palek@netguru.pl>
2015-01-18 19:44:50 +0100

    add simple list component

---
+++ tests/unit/components/pp-list-test.js
@@ -0,0 +1,52 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
+
+var compile = Ember.Handlebars.compile;
+var View = Ember.View;
+var run = Ember.run;
+
+var wrapperView;
+
+moduleForComponent('pp-list', 'pp-list component', {
+  teardown: function() {
+    if(wrapperView) {
+      run(function(){
+        wrapperView.destroy();
+      });
+    }
+
+    wrapperView = null;
+  }
+});
+
+test('it renders', function() {
+  expect(2);
+
+  // creates the component instance
+  var component = this.subject();
+  equal(component._state, 'preRender');
+
+  // appends the component to the page
+  this.append();
+  equal(component._state, 'inDOM');
+});
+
+test('it renders the passed items', function(){
+  wrapperView = View.create({
+    container: this.container,
+    list: [1, 2, 3],
+    template: compile(
+      '{{#pp-list collection=view.list as |item|}}' +
+        '<span>{{item}}</span>' +
+      '{{/pp-list}}'
+    )
+  });
+
+  run(function(){
+    wrapperView.appendTo('#ember-testing-container');
+  });
+
+  var text = wrapperView.$('span').text();
+
+  equal(text, '123');
+});
